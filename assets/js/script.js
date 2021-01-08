cbdccbb1cfbd2f39a1b0632f955aa6cc35f09020
ilechuks73 <62202169+ilechuks73@users.noreply.github.com>
2021-01-08 15:43:46 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,70 @@
+var hours = document.getElementById('hour')
+var mins = document.getElementById('minute')
+var secs = document.getElementById('second')
+var mss = document.getElementById('milisecond')
+
+var startbtn = document.getElementById('start')
+var pausebtn = document.getElementById('pause')
+var resumebtn = document.getElementById('resume')
+var resetbtn = document.getElementById('reset')
+
+var dt = 1
+
+var timer
+
+startbtn.addEventListener('click', function () {
+    startbtn.setAttribute('disabled', 'true')
+    pausebtn.removeAttribute('disabled')
+    var ms = 0
+    var sec = 0
+    var min = 0
+    var hour = 0
+    dt = 1
+    timer = setInterval(() => {
+
+        ms += dt;
+        if (ms == 100) {
+            ms = 0
+            sec += 1
+            if (sec > 59) {
+                sec = 0
+                min += 1
+                if (min > 59) {
+                    min = 0
+                    hour += 1
+                }
+            }
+        }
+        mss.innerText = ms
+        secs.innerText = sec
+        mins.innerText = min
+        hours.innerText = hour
+    }, 10);
+
+
+});
+
+pausebtn.addEventListener('click', function () {
+    dt = 0;
+    pausebtn.classList.toggle('hide')
+    resumebtn.classList.toggle('hide')
+})
+
+resumebtn.addEventListener('click', function () {
+    dt = 1
+    pausebtn.classList.toggle('hide')
+    resumebtn.classList.toggle('hide')
+})
+
+resetbtn.addEventListener('click', function () {
+    clearInterval(timer)
+    startbtn.removeAttribute('disabled')
+    pausebtn.setAttribute('disabled', 'true')
+    pausebtn.classList.remove('hide')
+    resumebtn.classList.add('hide')
+
+    mss.innerText = 0
+    secs.innerText = 0
+    mins.innerText = 0
+    hours.innerText = 0
+})
